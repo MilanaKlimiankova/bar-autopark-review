698fd26294afea2bfb5acc2aa6ce4afb1e31e1ef
Мiлана Клiмянкова <40536010+MilanaKlimiankova@users.noreply.github.com>
2024-07-19 08:41:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-alert('Работает!');
+const urlParams = new URLSearchParams(window.location.search);
+const message = urlParams.get('message');
+document.getElementById('message').innerText = message;
