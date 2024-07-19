9f4d21cc2e0f90c1a07ed1f62b1fa52851cb2035
Мiлана Клiмянкова <40536010+MilanaKlimiankova@users.noreply.github.com>
2024-07-19 09:25:01 +0300

    Update send_review.php

--- send_review.php
+++ send_review.php
@@ -1,9 +1,16 @@
 <?php
-  //$review = wordwrap($_POST['review'], 70, "\r\n");
+  header('Content-Type: text/html; charset=UTF-8');
+
+  $review = wordwrap($_POST['review'], 70, "\r\n");
   $review = 'test text';
   $to = 'klimenkovamilana@gmail.com';
   $subject = 'Review';
+  $headers = '';
 
-  mail($to, $subject, $review);
+  if (mail($to, $subject, $review, $headers)) {
+    echo 'Отзыв отправлен успешно!';
+} else {
+    echo 'Ошибка отправки отзыва!';
+}
 ?>
 
