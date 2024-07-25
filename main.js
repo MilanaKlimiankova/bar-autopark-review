307aaac7c47e806f044bb111a39e5b51deb71efa
Мiлана Клiмянкова <40536010+MilanaKlimiankova@users.noreply.github.com>
2024-07-25 10:02:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -32,30 +32,36 @@ function findBusByGarageNumber(garageNumber) {
   for (var i = 0; i < buses.length; i++) {
     var garageNumberElement = buses[i].getElementsByTagName("garage_number")[0];
     if (garageNumberElement.textContent == garageNumber) {
-      return buses[i];
+      //return buses[i];
+      var busInfo = "";
+  busInfo += "Гаражный номер: " + buses[i].getElementsByTagName("garage_number")[0].textContent + "<br>";
+  busInfo += "Модель автобуса: " + buses[i].getElementsByTagName("bus_model")[0].textContent + "<br>";
+  //busInfo += "Фото автобуса: <img src='" + bus.getElementsByTagName("bus_photo")[0].textContent + "'><br>";
+  busInfo += "Имя водителя: " + buses[i].getElementsByTagName("driver_name")[0].textContent;
+  document.getElementById("bus-info").innerHTML = busInfo;
     }
   }
   //return null;
 }
 
 // Функция для отображения информации об автобусе
-function displayBusInfo(bus) {
+/*function displayBusInfo(bus) {
   var busInfo = "";
   busInfo += "Гаражный номер: " + bus.getElementsByTagName("garage_number")[0].textContent + "<br>";
   busInfo += "Модель автобуса: " + bus.getElementsByTagName("bus_model")[0].textContent + "<br>";
   busInfo += "Фото автобуса: <img src='" + bus.getElementsByTagName("bus_photo")[0].textContent + "'><br>";
   busInfo += "Имя водителя: " + bus.getElementsByTagName("driver_name")[0].textContent;
   document.getElementById("bus-info").innerHTML = busInfo;
-}
+}*/
 
 // Поиск автобуса по гаражному номеру и отображение информации
 var garageNumber = "100230"; // замените на нужный гаражный номер
 var bus = findBusByGarageNumber(garageNumber);
-if (bus) {
+/*if (bus) {
   displayBusInfo(bus);
 } else {
   document.getElementById("bus-info").innerHTML = "Автобус не найден";
-}
+}*/
 
 
 /*
