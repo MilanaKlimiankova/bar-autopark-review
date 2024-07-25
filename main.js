f8634d3971ff9cd855d3decb1d8dea20cf79c004
Мiлана Клiмянкова <40536010+MilanaKlimiankova@users.noreply.github.com>
2024-07-25 10:51:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -62,5 +62,5 @@ function displayBusInfo(bus) {
 
   const photoDiv = document.getElementById('bus_photo');
   const photoURL = bus.getElementsByTagName('bus_photo')[0].textContent;
-  photoDiv.style.background = photoURL;
+  photoDiv.style.background = `url(${photoURL})`;
 }
